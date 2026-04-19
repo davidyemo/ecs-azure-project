6b1da49ee147c535d7e872da2e5dc951a86049e9
davidyemo <dyemo@outlook.com>
2026-04-19 17:03:55 +0100

    Adding project files

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "client_id" {
+  description = "The client ID of the Azure service principal."
+  type        = string
+}
+
+variable "client_secret" {
+    description = " The Client secret"
+    type = string
+}
+
+variable "tenant_id" {
+    description = " The Tenant ID"
+    type = string
+}
+
+variable "subscription_id" {
+    description = " The Subscription ID"
+    type = string
+}
