d48445dd8c0e8fadf8590588c6a682d9ea95a08c
davidyemo <dyemo@outlook.com>
2026-05-19 00:44:52 +0100

    Updated Terraform files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,12 +5,19 @@ terraform {
       version = "=4.68.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "ecs-rg1"
+    storage_account_name = "ecstfstate"
+    container_name       = "content"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
