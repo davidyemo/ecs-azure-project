cd35726c1afe1abb1e4dd4a1147408dd74c62a1c
davidyemo <dyemo@outlook.com>
2026-05-30 12:17:32 +0100

    Test Terraform Plan Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
 }
-##
+###
 resource "azurerm_resource_group" "destroy_rg" {
   name     = "destroy_rg"
   location = "West Europe"
