e33d6b027281ebf352410702a542de953743ec4f
davidyemo <dyemo@outlook.com>
2026-06-07 00:25:56 +0100

    Removed Destroy Rg resource from Main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,6 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-# add a comment to main.tf
-
-resource "azurerm_resource_group" "destroy_rg" {
-  name     = "destroy_rg"
-  location = "West Europe"
-}
-
 # Creating Azure Container Registry
 
 module "acr" {
